b3180a4ad93743452806eb43a2bda7174984ab06
Ankur <ankurisawesome7@gmail.com>
2026-05-24 11:46:06 +0530

    Initial Commit

---
+++ Linkase/LinkaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkaseApp.swift
+//  Linkase
+//
+//  Created by Ankur Kumar on 24/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LinkaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
